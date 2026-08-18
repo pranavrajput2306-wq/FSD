5651a20278a8cd8ee61c71af57e2dc79bd705f22
pranavrajput2306-wq <pranavrajput2306@gmail.com>
2026-08-18 09:34:27 +0530

    basics html

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+//document.write
+//inner html
+//console.log
+document.write("hello world");
+document.write("pranav");
+alert("hello");
+// why alert at first-because it is syncronous (async await tech) blocking function
+console.log("hello");
+// backend se data fetch krne ke liye
+let p=document.querySelector("p");
+p.innerHTML="<i>hello</i>"
